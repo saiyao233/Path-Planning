87ea1cd2a4e3681380279164ae5e0e42cd638bdc
saiyao233 <better_man_ll@163.com>
2024-06-07 00:05:51 +0800

    作业

---
+++ build/CMakeFiles/spacelib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spacelib.dir/space.cu.o"
+  "CMakeFiles/spacelib.dir/space.cu.o.d"
+  "libspacelib.a"
+  "libspacelib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/spacelib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
